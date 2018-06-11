448070558835f9e82dd59d556f6d3c31c5826d91
osnipr@gmail.com <osnipr@gmail.com>
2018-06-11 19:24:50 -0300

    Versao aulas completo

---
+++ Funcionarios/Funcionarios/Endereco.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Funcionarios
+{
+    public class Endereco
+    {
+        private string logradouro;
+        private string cep;
+        private string bairro;
+        private string cidade;
+        private string estado;
+        private string complemento;
+        private string numero;
+
+        public Endereco()
+        {
+
+        }
+
+        public string Logradouro { get => logradouro; set => logradouro = value; }
+        public string Cep { get => cep; set => cep = value; }
+        public string Bairro { get => bairro; set => bairro = value; }
+        public string Cidade { get => cidade; set => cidade = value; }
+        public string Estado { get => estado; set => estado = value; }
+        public string Complemento { get => complemento; set => complemento = value; }
+        public string Numero { get => numero; set => numero = value; }
+    }
+}
---
+++ Funcionarios/Funcionarios/Funcionario.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Funcionarios
+{
+    public class Funcionario
+    {
+        private string nome;
+        private string cargo;
+        private double salario;
+
+        public Funcionario()
+        {
+
+        }
+
+        public string Nome { get => nome; set => nome = value; }
+        public string Cargo { get => cargo; set => cargo = value; }
+        public double Salario { get => salario; set => salario = value; }
+    }
+}
